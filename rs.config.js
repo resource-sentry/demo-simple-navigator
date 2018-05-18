9e8bc340ff61f33ee017f96dc9099a66b9fd4690
Nicolas Siver <nicolas@siver.im>
2018-05-17 23:47:58 -0400

    add resources

--- rs.config.js
+++ rs.config.js
@@ -1,9 +1,13 @@
-const ReaderSvg    = require('@resource-sentry/reader-svg'),
-      WriterEs2015 = require('@resource-sentry/writer-es2015');
+const ReaderProperties = require('@resource-sentry/reader-properties'),
+      ReaderSvg        = require('@resource-sentry/reader-svg'),
+      WriterEs2015     = require('@resource-sentry/writer-es2015');
 
 module.exports = {
     config: {
         input : [
+            new ReaderProperties({
+                entry: './data/heroes.properties'
+            }),
             new ReaderSvg({
                 entry: 'asset'
             })
@@ -11,9 +15,5 @@ module.exports = {
         output: new WriterEs2015({
             path: './src/model'
         })
-    },
-
-    settings: {
-        watch: true
     }
 };
