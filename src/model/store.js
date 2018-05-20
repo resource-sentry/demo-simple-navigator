371f983c664d2306dbf975a222573f0ca3be6b10
Nicolas Siver <nicolas@siver.im>
2018-05-20 18:26:01 -0400

    add bio resources to the storage

--- src/model/store.js
+++ src/model/store.js
@@ -9,35 +9,43 @@ export function createInitialState() {
         menuItems    : createMenuItems([
             {
                 asset: Rs.Graphic.CAPTAIN_AMERICA_ADAMANTIUM_SHIELD,
-                label: 'Captain America'
+                label: 'Captain America',
+                bio  : Rs.Text.MEH_CAPTAIN_BIO
             },
             {
                 asset: Rs.Graphic.DEADPOOL,
-                label: 'Deadpool'
+                label: 'Deadpool',
+                bio  : Rs.Text.COOL_DEADPOOL_BIO
             },
             {
                 asset: Rs.Graphic.GHOST_RIDER,
-                label: 'Ghost Rider'
+                label: 'Ghost Rider',
+                bio  : Rs.Text.COOL_GHOST_RIDER_BIO
             },
             {
                 asset: Rs.Graphic.IRON_MAN,
-                label: 'Iron Man'
+                label: 'Iron Man',
+                bio  : Rs.Text.COOL_IRON_MAN_BIO
             },
             {
                 asset: Rs.Graphic.LOKI,
-                label: 'Loki'
+                label: 'Loki',
+                bio  : Rs.Text.MEH_LOKI_BIO
             },
             {
                 asset: Rs.Graphic.PUNISHER,
-                label: 'Punisher'
+                label: 'Punisher',
+                bio  : Rs.Text.MEH_PUNISHER_BIO
             },
             {
                 asset: Rs.Graphic.SPIDERMAN,
-                label: 'Spider-Man'
+                label: 'Spider-Man',
+                bio  : Rs.Text.MEH_SPIDER_MAN_BIO
             },
             {
                 asset: Rs.Graphic.THOR_MJOLNIR,
-                label: 'Thor'
+                label: 'Thor',
+                bio  : Rs.Text.COOL_THOR_BIO
             }
         ])
     };
